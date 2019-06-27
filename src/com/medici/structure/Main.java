3ff83216eb61493a959f987feb554811ad1c80ef
ZongHao Li <mr.medici@foxmail.com>
2019-06-27 14:34:55 +0800

    commit dynamic array

--- src/com/medici/structure/Main.java
+++ src/com/medici/structure/Main.java
@@ -1,8 +1,14 @@
 package com.medici.structure;
 
+import com.medici.structure.linear.Array;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Array<Integer> array = new Array<>(5);
+        for (int index = 0; index < 5;index ++){
+            array.addLast(index);
+        }
+        System.out.println(array);
     }
 }
