1a186dfb7aa0d9888929ca49ec4114ca59e7f0f2
ZongHao Li <mr.medici@foxmail.com>
2019-07-31 18:04:40 +0800

    solution leetcode id 303 and id 307

---
+++ src/com/medici/structure/leetcode/solution303/NumArray.java
@@ -0,0 +1,28 @@
+package com.medici.structure.leetcode.solution303;
+/// 303. Range Sum Query - Immutable
+/// https://leetcode.com/problems/range-sum-query-immutable/description/
+
+import com.medici.structure.tree.segment.SegmentTree;
+
+class NumArray {
+
+    private SegmentTree<Integer> segmentTree;
+    public NumArray(int[] nums) {
+
+        if(nums.length > 0){
+            Integer[] data = new Integer[nums.length];
+            for (int i = 0; i < nums.length; i++)
+                data[i] = nums[i];
+            segmentTree = new SegmentTree<>(data, (a, b) -> a + b);
+        }
+
+    }
+
+    public int sumRange(int i, int j) {
+
+        if(segmentTree == null)
+            throw new IllegalArgumentException("Segment Tree is null");
+
+        return segmentTree.query(i, j);
+    }
+}
